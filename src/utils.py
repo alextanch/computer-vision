b7b626d5b7ebb70e1a01f8e57634f9f7e602cb13
Alex Tanchenko <alex.tanch@gmail.com>
2026-01-08 14:01:13 +0300

    [at] lesson 1

---
+++ src/utils.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from zipfile import ZipFile
+
+import gdown
+
+
+def download_and_extract(google_id, file, output_dir):
+    output_dir = Path(output_dir)
+    output_dir.mkdir(exist_ok=True)
+
+    file = Path(output_dir) / file
+
+    if not file.exists():
+        print(f"{file} downloading...")
+        gdown.download(
+            id=google_id,
+            output=str(file),
+            quiet=True
+        )
+    
+    if not file.with_suffix("").is_dir():
+        print("Extracting...")
+        with ZipFile(file, "r") as fp:
+            fp.extractall(path=output_dir)
+
+    print("Done!")
