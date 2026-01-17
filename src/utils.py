f83888fcca8a85b72f6019fbc89660b3957c975f
Alex Tanchenko <alex.tanch@gmail.com>
2026-01-17 14:18:59 +0300

    at: update lesson2

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+from zipfile import ZipFile
+
+import gdown
 import torch
 
 
@@ -11,3 +15,25 @@ def set_seed(seed):
         # что бы все операции на GPU были детерменированными
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
+
+
+def download_and_extract(google_id, file, output_dir):
+    output_dir = Path(output_dir)
+    output_dir.mkdir(exist_ok=True)
+
+    file = Path(output_dir) / file
+
+    if not file.exists():
+        print(f"{file} downloading...")
+        gdown.download(
+            id=google_id,
+            output=str(file),
+            quiet=True
+        )
+    
+    if not file.with_suffix("").is_dir():
+        print("Extracting...")
+        with ZipFile(file, "r") as fp:
+            fp.extractall(path=output_dir)
+
+    print("Done!")
